5b59dd484877f91b5d60d4de14df27ec96b62157
Elena Mayer <elena.mayer@rebelmouse.com>
2017-06-07 11:08:56 +0700

    Yii2

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=shops-gid',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
